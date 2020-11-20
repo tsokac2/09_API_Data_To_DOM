c6126da1b93faf44ea43899d5fbf3c04ca11b438
tsokac2 <tsokac2@gmail.com>
2020-11-20 10:17:22 +0000

    Add tableHeaders to app.js

--- js/app.js
+++ js/app.js
@@ -1,37 +1,40 @@
 const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
 function getData(type, cb) {
-
     var xhr = new XMLHttpRequest();
-    var data;
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
-
-    function setData (jsonData) {
-        data = jsonData;
-        console.log(data);
-    }
-
-
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText)); 
         }
     };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 };
+
+function getTableHeaders(obj) {
+    let tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key){
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
 
 function writeToDocument(type) {
    let el = document.getElementById("data");
    el.innerHTML = "";
    getData(type, function(data){
         data = data.results;
+        let tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item){
-            Object.keys(item).forEach(function(key){
-                console.log(key);
-            })
             el.innerHTML += "<p>" + item.name + "</p>";
         });
+            
+        el.innerHTML = `<table>${tableHeaders}</table>`;
+
    });
 }
 
